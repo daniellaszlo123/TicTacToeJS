a6cf35d681f9665698ed8283f753b9a05ee2e1c0
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-03-22 12:12:17 +0100

    második verzió(még hibás)

--- script.js
+++ script.js
@@ -32,17 +32,29 @@ function megjelenit() {
 }
 
 var kiJon=0;
+var felulet=["-", "-", "-", "-", "-", "-", "-", "-", "-"];
 function xVagyO(){
     index=event.target.id;
     if (kiJon<1) {
         ID(index).innerHTML="<p>X</p>";
         ID(index).removeEventListener("click", xVagyO);
+        felulet[index]="X";
         kiJon++;
         //console.log(kiJon);
+        console.log(felulet);
     } else {
         ID(index).innerHTML="<p>O</p>";
         ID(index).removeEventListener("click", xVagyO);
+        felulet[index]="O";
         kiJon--;
         //console.log(kiJon);
+        console.log(felulet);
+    }
+
+    if (felulet[0]===felulet[1]===felulet[2] || felulet[3]===felulet[4]===felulet[5] || 
+        felulet[6]===felulet[7]===felulet[8] || felulet[0]===felulet[4]===felulet[6] ||
+        felulet[1]===felulet[5]===felulet[7] || felulet[2]===felulet[6]===felulet[8] ||
+        felulet[0]===felulet[5]===felulet[8] || felulet[3]===felulet[5]===felulet[6]) {
+        console.log("nyertél");
     }
 }
